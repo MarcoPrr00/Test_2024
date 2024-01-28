f2929c2a6a9d058e8852309d178e22f745d8b6df
MarcoPrr00 <m.porro14@studenti.uniba.it>
2024-01-28 15:37:37 +0100

    Test iniziali conversioni

--- OperazioniMathTest.java
+++ OperazioniMathTest.java
@@ -6,6 +6,22 @@ class OperazioniMathTest {
     OperazioniMath operazioniMath = new OperazioniMath();
 
     @Test
+    void testConversioneBinaria() {
+        assertEquals("1110", operazioniMath.convertiBase(14, 2));
+    }
+
+    @Test
+    void testConversioneOttale() {
+        assertEquals("33", operazioniMath.convertiBase(27, 8));
+    }
+
+    @Test
+    void testConversioneEsadecimale() {
+        assertEquals("FF", operazioniMath.convertiBase(255, 16));
+    }
+
+
+    @Test
     void testEquazioneDueSoluzioniReali() {
         assertArrayEquals(new double[]{2.0, 1.0}, operazioniMath.calcolaSoluzioniEquazioneSecondoGrado(1, -3, 2));
     }
